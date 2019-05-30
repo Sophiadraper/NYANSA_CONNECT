2c65194a1fc073d61f0c641205bcf18f72f6bc80
Tiffany Sika-Dufie <tiffany.sika.dufie@outlook.com>
2019-05-30 18:33:55 +0100

    initial commit

--- itsforthekids.py
+++ itsforthekids.py
@@ -18,3 +18,11 @@ def hello_someone(name):
 return render_template("itsforthekids.html", name=name.title())
 
 app.run(debug=True)
+def send_simple_message():
+	return requests.post(
+		"https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+		auth=("api", "fe701f7c75c69830c5d85c787cc9555c-52b0ea77-c0328086"),
+		data={"from": "Excited User <mailgun@https://api.mailgun.net/v3/sandboxb471aba1ce164154a7799313792615aa.mailgun.org>",
+			"to": ["bar@example.com", "YOU@YOUR_DOMAIN_NAME"],
+			"subject": "Hello",
+			"text": "Testing some Mailgun awesomness!"})
