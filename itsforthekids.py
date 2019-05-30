8ad21ed675ab0c016d2195f69b0aaefe785f144a
Tiffany Sika-Dufie <tiffany.sika.dufie@outlook.com>
2019-05-30 08:04:02 +0100

    python maps

--- itsforthekids.py
+++ itsforthekids.py
@@ -2,4 +2,19 @@
 def sign_up():
 form_data = request.form
 print form_data["email"]
-return "All OK"
+return "Thank you for subscribing!"
+
+@app.route("/<name>")
+def hello_someone(name):
+return render_template("itsforthekids.html", name=name.title())
+
+app = Flask("MyApp")
+@app.route("/")
+def hello():
+return "itsforthekids.html"
+@app.route("/<name>")
+def hello_someone(name):
+
+return render_template("itsforthekids.html", name=name.title())
+
+app.run(debug=True)
